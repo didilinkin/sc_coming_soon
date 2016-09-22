52c8951986525c8c136ee7a95e11da00e651b38b
yanxiaodi <929213769@qq.com>
2016-09-22 15:13:21 +0800

    完成icon上面的提示标题展示样式，下面需要给它添加点击事件 提前备份

--- app/jsx/icon.jsx
+++ app/jsx/icon.jsx
@@ -6,19 +6,52 @@ var IconComponent = React.createClass({
         return(
             <icon>
                 <div className="icon__box">
-                    <icontitle>     {/* 虚拟出来的标签——包住4个标题文字 */}
+                    {/* 虚拟出来的标签——包住4个标题文字 */}
+                    {/* <icontitle>
                         <ul className="icon__ul">
                             <li><p>1234567890</p></li>
                             <li><p>0987654321</p></li>
                             <li><p>1234567890</p></li>
                             <li><p>1234567890</p></li>
                         </ul>
-                    </icontitle>
+                    </icontitle> */}
                     <box>   {/* 虚拟出来的标签——包住4个i标签 */}
-                        <i className="fa fa-qq"></i>
-                        <i className="fa fa-weixin"></i>
-                        <i className="fa fa-phone"></i>
-                        <i className="fa fa-at"></i>
+                        <title-icon>    {/* 标题文字 与 icon图标的集合 */}
+                            <title-icon__title>     {/* 文字盒子 */}
+                                <p>1234567890</p>
+                            </title-icon__title>
+                            <i__box>
+                                <i className="fa fa-qq"></i>
+                            </i__box>
+                        </title-icon>
+
+                        <title-icon>    {/* 标题文字 与 icon图标的集合 */}
+                            <title-icon__title>     {/* 文字盒子 */}
+                                <p>1234567890</p>
+                            </title-icon__title>
+                            <i__box>
+                                <i className="fa fa-weixin"></i>
+                            </i__box>
+                        </title-icon>
+
+                        <title-icon>    {/* 标题文字 与 icon图标的集合 */}
+                            <title-icon__title>     {/* 文字盒子 */}
+                                <p>1234567890</p>
+                            </title-icon__title>
+                            <i__box>
+                                <i className="fa fa-phone"></i>
+                            </i__box>
+                        </title-icon>
+
+                        <title-icon>    {/* 标题文字 与 icon图标的集合 */}
+                            <title-icon__title>     {/* 文字盒子 */}
+                                <p>1234567890</p>
+                            </title-icon__title>
+                            <i__box>
+                                <i className="fa fa-at"></i>
+                            </i__box>    
+                        </title-icon>
+                        {/* 这个内容列表可以考虑使用数组的方法来调用 */}
                     </box>
                 </div>
                 <div className="icon__flex"></div>
